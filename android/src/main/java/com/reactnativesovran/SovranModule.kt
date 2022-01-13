f8decf08545105cf3a57401d29d2cf1e79995169
Oscar Bazaldua <oscar.bazaldua@outlook.com>
2022-01-13 10:24:55 -0600

    feature: android native support

--- android/src/main/java/com/reactnativesovran/SovranModule.kt
+++ android/src/main/java/com/reactnativesovran/SovranModule.kt
@@ -1,24 +1,53 @@
 package com.reactnativesovran
 
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.modules.core.DeviceEventManagerModule
 
 class SovranModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    override fun getName(): String {
-        return "Sovran"
-    }
+  var onInitialized: () -> Unit = {}; 
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
-    }
+  override fun getName(): String {
+    return "Sovran"
+  }
 
-    
+  companion object {
+    const val ON_STORE_ACTION = "onStoreAction"
+  }
+
+  override fun getConstants() = mapOf(
+      "onStoreAction" to ON_STORE_ACTION
+  )
+
+  override fun initialize() {
+    super.initialize()
+    onInitialized()
+  }
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  fun addListener(eventName: String?) {
+    // Keep: Required for RN built in Event Emitter Calls.
+  }
+
+  @ReactMethod
+  fun removeListeners(count: Int?) {
+    // Keep: Required for RN built in Event Emitter Calls.
+  }
+
+  fun dispatch(action: String, payload: Map<String, Any?>) {
+    val map = mapOf(
+        "type" to action,
+        "payload" to payload
+      )
+      val event: WritableMap = Arguments.makeNativeMap(map)
+      reactApplicationContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+        .emit(ON_STORE_ACTION, event)
+  }
 }
